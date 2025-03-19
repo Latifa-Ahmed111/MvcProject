99926bdd54623a0c00057a390629deadb1209748
Latifa-Ahmed111 <latifaa.hanfy@gmail.com>
2025-03-19 21:28:43 +0200

    2.Department Module With Configurations

---
+++ IKEA.DATA/Models/Departments/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKEA.DAl.Models.Departments
+{
+    public class Department : ModelBase
+    {
+
+        public string Name { get; set; } = null !;
+
+        public string Code { get; set; } = null!;
+
+        public string? Description { get; set; } 
+
+        public DateOnly CreationDate { get; set; }
+
+    }
+}
---
+++ IKEA.DATA/Persistance/Data/ApplicationDbcontext.cs
@@ -0,0 +1,28 @@
+﻿using IKEA.DAl.Models.Departments;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKEA.DAl.Persistance.Data
+{
+    public class ApplicationDbcontext:DbContext
+    {
+
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+
+
+        public DbSet <Department> Department { get; set; }
+
+    }
+}
