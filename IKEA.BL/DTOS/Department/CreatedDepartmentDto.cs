fcbfc9fc4a3ea67989ed66d08e035d9f9b96a979
Latifa-Ahmed111 <latifaa.hanfy@gmail.com>
2025-04-30 16:21:19 +0300

    Client Sude Validation

--- IKEA.BL/DTOS/Department/CreatedDepartmentDto.cs
+++ IKEA.BL/DTOS/Department/CreatedDepartmentDto.cs
@@ -12,7 +12,7 @@ namespace IKEA.BLL.DTOS.Department
         [Required(ErrorMessage = "Name is Reequired !!")]
         public string Name { get; set; } = null!;
 
-        [Required(ErrorMessage ="Code is Reequired !!")]
+        [Required(ErrorMessage ="Code is Required !!")]
         public string Code { get; set; } = null!;
 
         public string? Description { get; set; }
